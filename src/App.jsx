9af20356e0899bb7443eab7c404987ef070f56d8
Fabiantric <59180725+fabiantric@users.noreply.github.com>
2023-10-03 16:53:44 -0500

    version 1

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: true
+  },
+  {
+    userName: 'FabianTrivinoc',
+    name: 'Fabian Triviño',
+    isFollowing: false
+  },
+  {
+    userName: 'ossia',
+    name: 'Quincy Larson',
+    isFollowing: true
+  },
+]
+ 
+export function App (){
+  return (
+    <section className="App">
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName} 
+            userName={userName} 
+            name={name}
+            initialIsFollowing={isFollowing}
+          />
+        ))
+      }     
+    </section>
+  )
+}
