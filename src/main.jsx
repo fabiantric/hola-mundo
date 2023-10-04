1706d97eef2fca97a1edd4c89adf99155468f617
Fabiantric <59180725+fabiantric@users.noreply.github.com>
2023-10-04 11:27:43 -0500

    Version 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { Articulo } from './Articulo'
+import { Banner } from './Banner'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  
+  <main className='main-container'>
+    <Articulo />
+    <Banner />
+  </main>
 )
